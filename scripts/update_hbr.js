3e3117ff32989a58cc70c460e093fb0a12e10dca
Xu Siyuan <inqb@protonmail.com>
2024-11-23 00:14:21 +0800

    fix: hbr json data can be not array

--- scripts/update_hbr.js
+++ scripts/update_hbr.js
@@ -54,20 +54,15 @@ async function main() {
     if (lastModified[file]) {
       headers['If-Modified-Since'] = lastModified[file]
     }
-    let response
     try {
-      response = await axios.get(`${DATA_HOST}/${file}`, {
+      let response = await axios.get(`${DATA_HOST}/${file}`, {
         headers,
         responseType: 'json',
         validateStatus: function (status) {
           return (status >= 200 && status < 300) || status === 304
         },
       })
-    } catch (error) {
-      console.error(`Error: ${error.message}`)
-      throw error
-    }
-    try {
+
       let data
       const fileTarget = join(DATA_LOCAL, file)
       if (response.status !== 304) {
@@ -78,55 +73,45 @@ async function main() {
         data = JSON.parse(readFileSync(fileTarget, 'utf-8'))
       }
 
-      await Promise.all(
-        data.flatMap((datum) =>
-          images.flatMap(async (imgType) => {
-            let source = datum?.[imgType] ?? ''
-            if (source === '') return []
+      if (data.flatMap && images.flatMap && images.flatMap.length > 0) {
+        await Promise.all(
+          data.flatMap((datum) =>
+            images.flatMap(async (imgType) => {
+              let source = datum?.[imgType] ?? ''
+              if (source === '') return []
 
-            source = `${IMG_HOST}/${datum[imgType]}`
-            const target = join(IMG_LOCAL, datum[imgType])
-            try {
-              await access(target)
-              return [] // file exists, skip
-            } catch {
-              // file not exists, fetch
-            }
+              source = `${IMG_HOST}/${datum[imgType]}`
+              const target = join(IMG_LOCAL, datum[imgType])
+              try {
+                await access(target)
+                return [] // file exists, skip
+              } catch {
+                // file not exists, fetch
+              }
 
-            console.log(`according to ${file}, fetching ${datum.label} ${imgType} '${source}`)
-            const response = await axios({
-              method: 'get',
-              url: source,
-              responseType: 'stream',
-            })
-
-            /** @type {WriteStream} */
-            const piper = response.data.pipe(createWriteStream(target))
-            return new Promise((resolve, reject) => {
-              piper.on('finish', () => {
-                resolve(true)
+              console.log(`according to ${file}, fetching ${datum.label} ${imgType} '${source}`)
+              const response = await axios({
+                method: 'get',
+                url: source,
+                responseType: 'stream',
               })
-              piper.on('error', () => {
-                reject()
-              })
-            })
-          }),
-        ),
-      )
 
-      // for (const image of images) {
-      //   try {
-      //     let response = await axios.get(`${IMAGE_PREFIX}/${response.data[image]}`, { responseType: 'stream' })
-      //     response.data.pipe(createWriteStream(`${HBR_IMG}/${response.data[image]}`))
-      //   } catch (error) {
-      //     console.error(`Error: ${error.message}`)
-      //     continue
-      //   }
-      // }
-    } catch (error) {
-      if (response !== undefined) {
-        console.error(`Error: ${error}`)
+              /** @type {WriteStream} */
+              const piper = response.data.pipe(createWriteStream(target))
+              return new Promise((resolve, reject) => {
+                piper.on('finish', () => {
+                  resolve(true)
+                })
+                piper.on('error', () => {
+                  reject()
+                })
+              })
+            }),
+          ),
+        )
       }
+    } catch (error) {
+      console.error(`Error fetching ${file}`, error.message ?? error)
       throw error
     }
   }
