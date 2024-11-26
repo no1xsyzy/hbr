d722082b3674d9b19a3f7428a81fcaacef83d8e3
Xu Siyuan <inqb@protonmail.com>
2024-11-26 15:09:50 +0800

    feat: StyleIcon show tier, type elements

--- src/components/StyleIcon.svelte
+++ src/components/StyleIcon.svelte
@@ -1,24 +1,48 @@
 <script lang="ts">
   import { styles } from '../lib/data.ts'
   import { translate } from '../lib/translate.ts'
+  import Symbol from './Symbol.svelte'
+  import type { Style } from '../lib/types'
+  import { translateType } from '../lib/utils'
+
   interface Props {
-    style: any
+    style: Style
     size?: string
     translates?: boolean
+    detailed?: boolean
   }
 
-  let { style, size = '2em', translates = true }: Props = $props()
+  let { style, size = '2em', translates = true, detailed = false }: Props = $props()
 
   let foundStyle = $derived(style?.label ? style : styles.find((s) => s.label === style))
   let src = $derived(foundStyle ? 'hbr/' + foundStyle.image : '')
   let alt = $derived(foundStyle ? (translates ? translate(foundStyle) : foundStyle).name : style)
 </script>
 
-<img style={'--size: ' + size} {src} {alt} />
+<div class="root" style={'--size: ' + size}>
+  <img {src} {alt} />
+  {#if detailed}
+    <div class="detail">
+      {#each [foundStyle.tier, ...foundStyle.elements, foundStyle.type] as el}
+        <Symbol symbol={translateType(el)} size={"calc("+size+" * 0.3)"}/>
+      {/each}
+    </div>
+  {/if}
+</div>
 
 <style>
-  img {
+  .root {
     height: var(--size);
     width: var(--size);
+    overflow: hidden;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .detail {
+    position: relative;
+    top: calc(0px - var(--size));
   }
 </style>
