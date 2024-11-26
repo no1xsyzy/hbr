f129032be40fd6d2aabb4dd44c3e4b3967d679db
Xu Siyuan <inqb@protonmail.com>
2024-11-26 15:14:57 +0800

    page: Config

--- src/components/Config.svelte
+++ src/components/Config.svelte
@@ -1,3 +1,28 @@
 <script>
-  import { prolog_init } from '../lib/config.svelte.ts'
+  import { prolog_init } from '../lib/config'
+  import { server } from '../lib/config'
+  import { prolog } from 'codemirror-lang-prolog'
+  import CodeMirror from 'svelte-codemirror-editor'
 </script>
+
+<div class="item">
+  <div class="label">Server</div>
+  <div class="input">
+    <input type="radio" bind:group={$server} value="WFS" /><label for="">WFS</label>
+    <input type="radio" bind:group={$server} value="BILI" /><label for="">BILI</label>
+  </div>
+</div>
+
+<div class="item">
+  <div class="label"><label for="">Prolog Init</label></div>
+  <div class="input"><CodeMirror bind:value={$prolog_init} lang={prolog()} /></div>
+</div>
+
+<style>
+  div.item {
+    display: grid;
+    grid-template-columns: 200px 1fr;
+    margin: 30px;
+    max-width: 1200px;
+  }
+</style>
